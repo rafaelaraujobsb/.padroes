a3f68c795a6a36fa323d5f4fd93ee1c8a32e43a3
Rafael Araujo <bsb.rafaelaraujo@gmail.com>
2019-10-14 23:13:11 -0300

    adicionando padrao pacote python

---
+++ pacote_python/run.py
@@ -0,0 +1,3 @@
+from nome_projeto import criar_app
+
+app = criar_app()
---
+++ pacote_python/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='nome_projeto',
+    version='0.1.0',
+    description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rafaelaraujobsb/.git',
+    author='Rafael Araujo',
+    author_email='bsb.rafaelaraujo@gmail.com',
+    packages=find_packages(exclude=['docs', 'tests']),
+    python_requires='>=3.6.*',
+    install_requires=[
+        'flask==1.1.1',
+        'loguru==0.3.2',
+        'flasgger==0.9.3',
+        'gunicorn==19.9.0',
+        'requests==2.22.0',
+        'jsonschema==2.6.0',
+        'flask_restful==0.3.7',
+        'beautifulsoup4==4.8.0',
+    ],
+)
